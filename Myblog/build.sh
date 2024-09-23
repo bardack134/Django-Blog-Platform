5512a761739973e26f85c72a2636360e84586415
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-09-23 10:14:54 +0900

    build.sh changes

--- Myblog/build.sh
+++ Myblog/build.sh
@@ -5,7 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-
+python manage.py migrate
+python manage.py makemigrations
 
 # Crea el superusuario
 python -c "
